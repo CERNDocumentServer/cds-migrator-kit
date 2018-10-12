d6d40f6c812d5a45e3d3739a09deb5bdc511ba6f
Nicola Tarocco <ntarocco@gmail.com>
2018-10-12 17:46:18 +0200

    wip: libraries migration

--- cds_migrator_kit/circulation/items/cli.py
+++ cds_migrator_kit/circulation/items/cli.py
@@ -34,14 +34,43 @@ class LibrariesMigrator():
 
     """
 
+    INVALID_LIBRARY_IDS = [43]
+    VALID_LIBRARY_TYPES = ['main', 'internal']
+
     def __init__(self, libraries):
         """Constructor."""
-        self.libraries = libraries
+        self.libraries = [l for l in libraries
+                          if l['type'] in self.VALID_LIBRARY_TYPES and
+                          l['id'] not in self.INVALID_LIBRARY_IDS]
+
+    def _migrate_internal_libraries(self, location_pid):
+        """Return new internal libraries records."""
+        internal_libraries = []
+        for i, library in enumerate(self.libraries):
+            internal_library = {
+                'libid': i,
+                'locid': location_pid,
+                'legacy_id': library['id'],
+                'name': library['name'],
+                'address': library.get('address', ''),
+                'email': library.get('email', ''),
+                'phone': library.get('phone', ''),
+                'notes': library.get('notes', ''),
+            }
+            internal_libraries.append(internal_library)
+        return internal_libraries
 
     def migrate(self):
-        """Create new libraries record from the legacy libraries."""
-        for library in self.libraries:
-            pass
+        """Return location and internal libraries records."""
+        location_pid = '1'
+        internal_libraries = self._migrate_internal_libraries(location_pid)
+
+        location = {
+            'locid': '1',
+            'name': 'CERN Central Library',
+        }
+
+        return location, internal_libraries
 
 
 @click.command()
