ed098328b2ce75d1b0bc8d1261c220a88eddf448
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2025-08-05 14:11:52 +0300

    sy rules: verified ignored fields

--- cds_migrator_kit/rdm/records/transform/models/sy.py
+++ cds_migrator_kit/rdm/records/transform/models/sy.py
@@ -23,15 +23,13 @@ class SYModel(CdsOverdo):
         "0248_q",
         "100__m",  # email of contributor
         "300__a",  # number of pages
-        # "270__m",
         "700__m",  # email of contributor
-        # "690C_a",
-        # "8564_8",
-        # "8564_s",
-        # "8564_x",
-        # "8564_y",  # file description - done by files dump
-        # "937__c",
-        # "937__s",
+        "8564_8",  # file id
+        "8564_s",  # bibdoc id
+        "8564_x",  # icon thumbnails sizes
+        "8564_y",  # file description - done by files dump
+        "937__c",  # last modified by
+        "937__s",  # last modification date
         "960__a",  # base number
         "961__c",  # CDS modification tag # TODO
         "961__h",  # CDS modification tag # TODO
