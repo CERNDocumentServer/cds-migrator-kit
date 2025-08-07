46ab95676f414d917bf538251368c68809ad63f0
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2025-08-07 10:17:19 +0300

    bulletin: add rules

--- cds_migrator_kit/rdm/records/transform/models/bulletin_issue.py
+++ cds_migrator_kit/rdm/records/transform/models/bulletin_issue.py
@@ -20,24 +20,39 @@ class BulletinIssueModel(CdsOverdo):
     __ignore_keys__ = {
         "0248_a",
         "0248_p",
-        "110__a",
-        "246_1a",
-        "690C_a",
-        "520__b",
-        "773__y",
-        "773__n",
-        "773__p",
-        "773__c",
         "0248_q",
-        "8564_8",
-        "8564_s",
-        "8564_x",
-        "980__a",
+        "100__m",  # email of contributor
+        "110__a",  # corporate author, always CERN, safe to ignore
+        # "246_1a",
+        "300__a",  # number of pages
+        "520__b",  # en value, redundant with language
+        "590__b",  # staff association? value, redundant with language
+        # "690C_a",
+        "700__m",  # email of contributor
+        # "773__c",
+        # "773__n",
+        "773__p",  # title of the "CERN Bulletin" series
+        "773__y",  # year, duplicate of 260
+        "787__i",  # one referenced record (video in 1755835, 1754359)
+        "8560_f",  # contact email
+        "8564_8",  # file id
+        "8564_s",  # bibdoc id
+        "8564_x",  # icon thumbnails sizes
+        "8564_y",  # file description - done by files dump
+        "937__c",  # last modified by
+        "937__s",  # last modification date
+        "960__a",  # base number
+        "961__c",  # CDS modification tag # TODO
+        "961__h",  # CDS modification tag # TODO
+        "961__l",  # CDS modification tag # TODO
+        "961__x",  # CDS modification tag # TODO
+        "980__a",  # same collection tag for everything
+        "981__a",  # duplicate record id
     }
 
     _default_fields = {
         "resource_type": {"id": "publication-other"},
-        "custom_fields": {},
+        "custom_fields": {"journal:journal": {"title": "CERN Bulletin"}},
         "creators": [{"person_or_org": {"type": "organizational", "name": "CERN"}}],
     }
 
--- cds_migrator_kit/rdm/records/transform/xml_processing/rules/bulletin_issue.py
+++ cds_migrator_kit/rdm/records/transform/xml_processing/rules/bulletin_issue.py
@@ -2,5 +2,93 @@ from dateutil.parser import ParserError, parse
 from dojson.errors import IgnoreKey
 
 from cds_migrator_kit.errors import UnexpectedValue
+from cds_migrator_kit.transform.xml_processing.quality.decorators import for_each_value
+from cds_migrator_kit.transform.xml_processing.quality.parsers import StringValue
+from .base import created
 
-from ...models.yellow_reports import yellow_issue_model as model
+from ...models.bulletin_issue import bull_issue_model as model
+
+
+@model.over("collection", "^690C_")
+@for_each_value
+def collection(self, key, value):
+    """Translates collection field."""
+    collection = value.get("a").strip().lower()
+    if collection not in ["cern", "cern bulletin printable version"]:
+        raise UnexpectedValue(subfield="a", key=key, value=value, field="690C_")
+    raise IgnoreKey("collection")
+
+
+@model.over("publication_date", "(^260__)", override=True)
+def imprint_info(self, key, value):
+    """Translates imprint - WARNING - also publisher and publication_date.
+
+    In case of summer student notes this field contains only date
+    but it needs to be reimplemented for the base set of rules -
+    it will contain also imprint place
+    """
+    _custom_fields = self.get("custom_fields", {})
+    imprint = _custom_fields.get("imprint:imprint", {})
+
+    publication_date_str = value.get("c")
+    _publisher = value.get("b")
+    place = value.get("a")
+    if _publisher and not self.get("publisher"):
+        self["publisher"] = _publisher
+    if place:
+        imprint["place"] = place
+    self["custom_fields"]["imprint:imprint"] = imprint
+    if publication_date_str:
+        try:
+            date_obj = parse(publication_date_str)
+            return date_obj.strftime("%Y-%m-%d")
+        except (ParserError, TypeError) as e:
+            raise UnexpectedValue(
+                field=key,
+                value=value,
+                message=f"Can't parse provided publication date. Value: {publication_date_str}",
+            )
+    raise IgnoreKey("publication_date")
+
+
+@model.over("custom_fields", "(^773__)")
+def journal(self, key, value):
+    _custom_fields = self.get("custom_fields", {})
+    journal_fields = _custom_fields.get("journal:journal", {})
+
+    journal_fields["issue"] = StringValue(value.get("n", "")).parse()
+    journal_fields["pages"] = StringValue(value.get("c", "")).parse()
+
+    _custom_fields["journal:journal"] = journal_fields
+    return _custom_fields
+
+
+@model.over("custom_fields_journal", "(^916__)", override=True)
+def issue_number(self, key, value):
+    _custom_fields = self.get("custom_fields", {})
+
+    issue = value.get("z")
+
+    journal_fields = _custom_fields.get("journal:journal", {})
+    journal_fields["issue"] = issue
+
+    _custom_fields["journal:journal"] = journal_fields
+    # because we override 916
+    self["status_week_date"] = created(self, key, value)
+
+    self["custom_fields"] = _custom_fields
+    raise IgnoreKey("custom_fields_journal")
+
+@model.over("custom_fields", "(^925__)")
+def issue_number(self, key, value):
+    _custom_fields = self.get("custom_fields", {})
+
+    issue_start = value.get("a")
+    issue_end = value.get("b")
+
+    issue = f"{issue_start}-{issue_end}"
+    journal_fields = _custom_fields.get("journal:journal", {})
+    journal_fields["issue"] = issue
+
+    _custom_fields["journal:journal"] = journal_fields
+    return _custom_fields
