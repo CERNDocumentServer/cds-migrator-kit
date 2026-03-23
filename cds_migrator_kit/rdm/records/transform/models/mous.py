e7ece36c1d411d6423c1ad24c6fc4def53e37307
Zübeyde Civelek <zubeydeecivelek@gmail.com>
2026-03-23 18:53:46 +0100

    add(mous): new model and transformation rules

--- cds_migrator_kit/rdm/records/transform/models/mous.py
+++ cds_migrator_kit/rdm/records/transform/models/mous.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2025 CERN.
+# Copyright (C) 2026 CERN.
 #
 # CDS-RDM is free software; you can redistribute it and/or modify it under
 # the terms of the MIT License; see LICENSE file for more details.
 
-"""CDS-RDM CMS note model."""
+"""CDS-RDM MoUs model."""
 from cds_migrator_kit.rdm.records.transform.models.base_record import (
     rdm_base_record_model,
 )
@@ -17,11 +17,19 @@ class MOUSModel(CdsOverdo):
 
     __query__ = "980__:MOUS"
 
-    __ignore_keys__ = {}
+    __ignore_keys__ = {
+        "8564_8",
+        "8564_s",
+        "8564_x",
+        "8564_y",  # file description - done by files dump
+        "8564_z",
+    }
 
     _default_fields = {
         "resource_type": {"id": "publication-other"},
         "custom_fields": {},
+        "dates": [],
+        "creators": [{"person_or_org": {"type": "organizational", "name": "CERN"}}],
     }
 
 
